f28c8607bcfd11b9ac044d79a0d393973c1bdc78
Danta1st <Danta1st@gmail.com>
2014-03-20 14:02:34 +0100

    Added a clock timer to the Build. - Something is not on time with the spawning mechanism! - Time between spawns are not consistent!

---
+++ Med10Project/Assets/ClockHandler.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ClockHandler : MonoBehaviour {
+
+	private float time = 2.0f;
+	private float individualTime;
+	private List<Transform> components = new List<Transform>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		GetChildTransforms();
+		CalculateIndividualTime();
+		HideComponents();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	#region Public Methods
+	public void SetTime(float t)
+	{
+		time = t;
+		CalculateIndividualTime();
+	}
+
+	public void BeginClock()
+	{
+		//Call the clock as a coroutine
+		StartCoroutine_Auto(DoClock());
+	}
+	#endregion
+
+	#region Class Methods
+	private void HideComponents()
+	{
+		//Scale the clock to size 0 - objects "goes into" the center object
+		iTween.ScaleTo(gameObject, Vector3.zero, 0.5f);
+
+		foreach(Transform t in components)
+		{
+			//Change alpha value to 25% over 2ms
+			iTween.FadeTo(t.gameObject, 0.25f, 0.2f);
+		}
+	}
+
+	private IEnumerator DoClock()
+	{
+		//Scale object to size 1 - Object "comes out" from the center object
+		iTween.ScaleTo(gameObject, Vector3.one, individualTime);
+
+		foreach(Transform t in components)
+		{
+			//Change alpha value to 100% over the weighted time.
+			iTween.FadeTo(t.gameObject, 1.0f, individualTime);
+			//Wait until first object is done fading
+			yield return new WaitForSeconds(individualTime);
+		}
+		//Hide all components when done
+		HideComponents();
+	}
+
+	private void GetChildTransforms()
+	{
+		foreach(Transform child in transform)
+		{
+			components.Add(child);
+		}
+	}
+
+	private void CalculateIndividualTime()
+	{
+		individualTime = time/components.Count;
+	}
+	#endregion
+}
