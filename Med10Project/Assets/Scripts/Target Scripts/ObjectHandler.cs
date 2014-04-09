4f25a2cd213e1e0bffda3fa26c37b1d4aa5b5896
Lars Holm Jespersen <larsholmjespersen@gmail.com>
2014-04-09 13:35:18 +0200

    Many changes to how we send data to logging.

--- Med10Project/Assets/Scripts/Target Scripts/ObjectHandler.cs
+++ Med10Project/Assets/Scripts/Target Scripts/ObjectHandler.cs
@@ -178,7 +178,7 @@ public class ObjectHandler : MonoBehaviour
 
 	private void DestroySelf()
 	{
-		soundManager.PlayMissed();
+		//soundManager.PlayMissed();
 		Destroy(gameObject);
 	}
 
@@ -219,7 +219,7 @@ public class ObjectHandler : MonoBehaviour
 				}
 
 				CalculateReactionTime();
-				txtWriter.LogData(reactiontime, transform.position, screenPos, true);
+				txtWriter.LogData(objectType.ToString(), reactiontime, angle, distance, transform.position, screenPos, true);
 
 				Unsubscribe();
 				Destroy(gameObject);
@@ -232,7 +232,7 @@ public class ObjectHandler : MonoBehaviour
 		isPunching = true;
 		Unsubscribe();
 		gameManager.ChangeCenterState(GameStateManager.State.awaitCenterClick);
-		txtWriter.LogData(reactiontime, transform.position, new Vector2(0,0), false);
+		txtWriter.LogData(objectType.ToString(), 0, angle, distance, transform.position, new Vector2(0,0), false);
 		sManager.ReportMiss(angleIdentifier, distance);
 		FadeOut(fadeOutTime);
 
