727cc2fc3c2b0573e55427f9e1a45aabf06119fd
Lars Holm Jespersen <larsholmjespersen@gmail.com>
2014-04-23 15:14:32 +0200

    Scatter plot done

--- Med10Project/Assets/Scripts/EndGameLines.cs
+++ Med10Project/Assets/Scripts/EndGameLines.cs
@@ -10,15 +10,18 @@ public class EndGameLines : MonoBehaviour {
 	private GameObject Grid;
 	private GameObject GridWithLabels;
 	private GameObject GridBG;
-
-
+	
 	private HighscoreManager hsManager;
+	private SpawnManager spManager;
 
-	private GameObject[] Nodes;
+	private GameObject[] NodeArray;
+	private GameObject[] ScatterArray;
 
 	private List<float> reactionMeans = new List<float>();
 
 	[SerializeField] private GameObject SpawnNode;
+	[SerializeField] private GameObject SpawnHit;
+	[SerializeField] private GameObject SpawnMiss;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +29,7 @@ public class EndGameLines : MonoBehaviour {
 		GridWithLabels = GameObject.Find("GridWithLabels");
 		GridBG = GameObject.Find("GridBG");
 		hsManager = GameObject.Find("HighscoreManager").GetComponent<HighscoreManager>();
+		spManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
 		lineRenderer = GetComponentInChildren<LineRenderer>();
 
 		Grid.SetActive(false);
@@ -48,22 +52,27 @@ public class EndGameLines : MonoBehaviour {
 	
 	}
 
-	private void EnableEndScreen()
+	private void EnableEndBackground()
 	{
-		GridWithLabels.SetActive(true);
 		GridBG.SetActive(true);
 	}
 
 	public void DisableEndScreen()
 	{
+		Grid.SetActive(false);
 		GridWithLabels.SetActive(false);
 		GridBG.SetActive(false);
 		lineRenderer.SetVertexCount(0);
+		DeleteOldNodes();
+		DeleteOldScatters();
 	}
 
-	public void DoHitMissScreen()
+	/*public void DoHitMissScreen()
 	{
-		EnableEndScreen();
+		GridWithLabels.SetActive(false);
+		Grid.SetActive(true);
+
+		EnableEndBackground();
 
 		float hits = 0;
 		float misses = 0;
@@ -87,11 +96,42 @@ public class EndGameLines : MonoBehaviour {
 		
 		StoreNodes();
 		DrawLines();
+	}*/
+
+	public void DoHitMissScreen()
+	{
+		DeleteOldScatters();
+		DeleteOldNodes();
+		GridWithLabels.SetActive(false);
+		Grid.SetActive(true);
+		
+		EnableEndBackground();
+		
+		for(int i = 1; i <= 10; i++)
+		{
+			List<float> angleHits = hsManager.GetHitDistances(i);
+			foreach (float hit in angleHits)
+			{
+				SpawnNodes(i, (hit/spManager.GetAbsMaxDist(i))*5, SpawnHit);
+			}
+
+			List<float> angleMisses = hsManager.GetMissDistances(i);
+			foreach (float miss in angleMisses)
+			{
+				SpawnNodes(i, (miss/spManager.GetAbsMaxDist(i))*5, SpawnMiss);
+			}
+		}
+		
+		StoreScatters();
 	}
 
 	public void DoReactionScreen()
 	{
-		EnableEndScreen();
+		DeleteOldNodes();
+		DeleteOldScatters();
+		GridWithLabels.SetActive(true);
+		Grid.SetActive(false);
+		EnableEndBackground();
 
 		GetReactions();
 
@@ -100,10 +140,10 @@ public class EndGameLines : MonoBehaviour {
 			int index = i -1;
 			if(reactionMeans[index] > 0.1f)
 			{
-				SpawnNodes(i, (reactionMeans[index]/2.5f)*5);
+				SpawnNodes(i, (reactionMeans[index]/2.5f)*5, SpawnNode);
 			}
 			else{
-				SpawnNodes(i, 0.1f);
+				SpawnNodes(i, 0.1f, SpawnNode);
 			}
 		}
 
@@ -116,41 +156,60 @@ public class EndGameLines : MonoBehaviour {
 		reactionMeans = hsManager.GetAllReactionTimes();
 	}
 
-	private void SpawnNodes(int int1to10, float distance)
+	private void SpawnNodes(int int1to10, float distance, GameObject spawnObject)
 	{
 		int angle = (36 * int1to10) - 18;
 		transform.Rotate(transform.forward, (float) -angle);
 		Quaternion rotation = transform.rotation;
 		Vector3 position = transform.position + gameObject.transform.up * distance;
 		transform.Rotate(transform.forward, (float) angle);
-		Instantiate(SpawnNode, position, rotation);
+		Instantiate(spawnObject, position, rotation);
 	}
 
 	private void StoreNodes()
 	{
-		Nodes = GameObject.FindGameObjectsWithTag("Node");
+		NodeArray = GameObject.FindGameObjectsWithTag("Node");
+	}
+
+	private void StoreScatters()
+	{
+		ScatterArray = GameObject.FindGameObjectsWithTag("ScatterPlot");
 	}
 
 	private void DrawLines()
 	{
-		lineRenderer.SetVertexCount(Nodes.Length+1);
-
-		for (int i = 0; i < Nodes.Length; i++) {
-			Vector3 pos = new Vector3(Nodes[i].transform.position.x, Nodes[i].transform.position.y, 1);
+		lineRenderer.SetVertexCount(NodeArray.Length+1);
+		
+		for (int i = 0; i < NodeArray.Length; i++) {
+			Vector3 pos = new Vector3(NodeArray[i].transform.position.x, NodeArray[i].transform.position.y, 1);
 			lineRenderer.SetPosition(i, pos);
 		}
-
-		Vector3 endPos = new Vector3(Nodes[0].transform.position.x, Nodes[0].transform.position.y, 1);
+		
+		Vector3 endPos = new Vector3(NodeArray[0].transform.position.x, NodeArray[0].transform.position.y, 1);
 		lineRenderer.SetPosition(10, endPos);
-
+		
 		DeleteOldNodes();
 	}
 
 	private void DeleteOldNodes()
 	{
-		foreach (GameObject node in Nodes)
+		if(NodeArray != null)
 		{
-			Destroy(node);
+			foreach (GameObject node in NodeArray)
+			{
+				Destroy(node);
+			}
+		}
+	}
+
+	private void DeleteOldScatters()
+	{
+		if(ScatterArray != null)
+		{
+			foreach (GameObject scatter in ScatterArray)
+			{
+				Destroy(scatter);
+			}
 		}
 	}
 }
