86bc5c44134f018a0c07a809f4279688967f2b3b
Danta1st <Danta1st@gmail.com>
2014-04-24 16:08:06 +0200

    Labels for grids

--- Med10Project/Assets/Scripts/EndGameLines.cs
+++ Med10Project/Assets/Scripts/EndGameLines.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class EndGameLines : MonoBehaviour {
+
+	[SerializeField] private bool useAdaptiveRtFigure = false;
+	[SerializeField] private GameObject Labels;
+	[SerializeField] private SpawnObjects spawnObjects;
 
 	private GameObject Grid;
 	private GameObject GridWithLabels;
@@ -13,14 +18,11 @@ public class EndGameLines : MonoBehaviour {
 
 	private GameObject[] NodeArray;
 	private GameObject[] ScatterArray;
+	private List<GameObject> LabelListGrid = new List<GameObject>();
+	private List<GameObject> LabelListAngles = new List<GameObject>();
 
 	private List<float> reactionMeans = new List<float>();
 
-	[SerializeField] private GameObject SpawnNode;
-	[SerializeField] private GameObject SpawnHit;
-	[SerializeField] private GameObject SpawnMiss;
-
-	// Use this for initialization
 	void Start () {
 		Grid = GameObject.Find("Grid");
 		GridWithLabels = GameObject.Find("GridWithLabels");
@@ -38,6 +40,49 @@ public class EndGameLines : MonoBehaviour {
 		GridBG.SetActive(true);
 	}
 
+	private void SpawnGridLabels(float incrementValue)
+	{
+		ClearLabelList(LabelListGrid);
+
+		for(int i = 1; i <= 5; i++)
+		{
+			GameObject go = SpawnNodeReturn(2, 1 * i, spawnObjects.NodeLabel);
+			//Set label text
+			go.GetComponent<NodeLabel>().SetText("" + incrementValue * i + "s");
+			go.transform.name += ""+i;
+			//Set parent to keep hierachy clean
+			go.transform.parent = Labels.transform;
+			//Stuff in array for cleaning later
+			LabelListGrid.Add(go);		
+		}
+	}
+
+	private void SpawnAngleLabels()
+	{
+		ClearLabelList(LabelListAngles);
+		
+		for(int i = 1; i <= 10; i++)
+		{
+			//Spawn angleID labels
+			GameObject go = SpawnNodeReturn(i, 5.5f, spawnObjects.AngleLabel);
+			//Set label text
+			go.GetComponent<NodeLabel>().SetText("" + (36 * i - 18));
+			go.transform.name = "AngleLabel"+i;
+			//Set parent to keep hierachy clean
+			go.transform.parent = Labels.transform;
+			//Stuff in array for cleaning later
+			LabelListAngles.Add(go);
+		}
+	}
+
+	private void ClearLabelList(List<GameObject> list)
+	{
+		foreach(GameObject go in list)
+			Destroy(go);
+
+		list.Clear();
+	}
+
 	public void DisableEndScreen()
 	{
 		Grid.SetActive(false);
@@ -51,23 +96,27 @@ public class EndGameLines : MonoBehaviour {
 	{
 		DeleteOldScatters();
 		DeleteOldNodes();
-		GridWithLabels.SetActive(false);
-		Grid.SetActive(true);
+		ClearLabelList(LabelListGrid);
 		
 		EnableEndBackground();
+		Grid.SetActive(true);
+
+		if(LabelListAngles.Count == 0)
+			SpawnAngleLabels();
+
 		
 		for(int i = 1; i <= 10; i++)
 		{
 			List<float> angleHits = hsManager.GetHitDistances(i);
 			foreach (float hit in angleHits)
 			{
-				SpawnNodes(i, (hit/spManager.GetAbsMaxDist(i))*5, SpawnHit);
+				SpawnNode(i, (hit/spManager.GetAbsMaxDist(i))*5, spawnObjects.SpawnHit);
 			}
 
 			List<float> angleMisses = hsManager.GetMissDistances(i);
 			foreach (float miss in angleMisses)
 			{
-				SpawnNodes(i, (miss/spManager.GetAbsMaxDist(i))*5, SpawnMiss);
+				SpawnNode(i, (miss/spManager.GetAbsMaxDist(i))*5, spawnObjects.SpawnMiss);
 			}
 		}
 		
@@ -78,21 +127,50 @@ public class EndGameLines : MonoBehaviour {
 	{
 		DeleteOldNodes();
 		DeleteOldScatters();
-		GridWithLabels.SetActive(true);
-		Grid.SetActive(false);
+		
 		EnableEndBackground();
+		Grid.SetActive(true);
 
-		GetReactions();
+		if(LabelListAngles.Count == 0)
+			SpawnAngleLabels();
+		
+		reactionMeans = hsManager.GetAllReactionTimes();
 
+		float incrementValue = 0.0f;
+
+		//Find the highest value reaction time
+		foreach(float value in reactionMeans)
+		{
+			if(value != null && value > incrementValue)
+				incrementValue = value;
+		}
+			
+		//What is the grid values? 
+		if(useAdaptiveRtFigure == true)
+		{
+			incrementValue = incrementValue/5.0f;
+		}
+		else if(incrementValue > 2.5f)
+		{
+			incrementValue = 1.0f;
+		}
+		else
+			incrementValue = 0.5f;
+
+		//Spawn grid labels
+		SpawnGridLabels(incrementValue);
+
+		//Spawn line nodes
 		for(int i = 1; i <= reactionMeans.Count; i++)
 		{
+
 			int index = i -1;
 			if(reactionMeans[index] > 0.1f)
 			{
-				SpawnNodes(i, (reactionMeans[index]/2.5f)*5, SpawnNode);
+				SpawnNode(i, (reactionMeans[index]/(incrementValue*5.0f))*5.0f, spawnObjects.SpawnNode);
 			}
 			else{
-				SpawnNodes(i, 0.1f, SpawnNode);
+				SpawnNode(i, 0.1f, spawnObjects.SpawnNode);
 			}
 		}
 
@@ -100,12 +178,7 @@ public class EndGameLines : MonoBehaviour {
 		DrawLines();
 	}
 
-	private void GetReactions()
-	{
-		reactionMeans = hsManager.GetAllReactionTimes();
-	}
-
-	private void SpawnNodes(int int1to10, float distance, GameObject spawnObject)
+	private void SpawnNode(int int1to10, float distance, GameObject spawnObject)
 	{
 		int angle = (36 * int1to10) - 18;
 		transform.Rotate(transform.forward, (float) -angle);
@@ -113,6 +186,17 @@ public class EndGameLines : MonoBehaviour {
 		Vector3 position = transform.position + gameObject.transform.up * distance;
 		transform.Rotate(transform.forward, (float) angle);
 		Instantiate(spawnObject, position, rotation);
+	}
+
+	private GameObject SpawnNodeReturn(int int1to10, float distance, GameObject spawnObject)
+	{
+		int angle = (36 * int1to10) - 18;
+		transform.Rotate(transform.forward, (float) -angle);
+		Quaternion rotation = transform.rotation;
+		Vector3 position = transform.position + gameObject.transform.up * distance;
+		transform.Rotate(transform.forward, (float) angle);
+		GameObject go = (GameObject) Instantiate(spawnObject, position, rotation);
+		return go;
 	}
 
 	private void StoreNodes()
@@ -157,4 +241,18 @@ public class EndGameLines : MonoBehaviour {
 			}
 		}
 	}
+	
+	#region Subclasses
+	[System.Serializable]
+	public class SpawnObjects
+	{
+		public GameObject SpawnNode;
+		public GameObject SpawnHit;
+		public GameObject SpawnMiss;
+		public GameObject NodeLabel;
+		public GameObject AngleLabel;
+
+	}
+	#endregion
+	
 }
